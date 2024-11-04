042ca50516992e75fbf5e7aefd722c6fd200c27c
haile <haileleulfiseha@gmail.com>
2024-11-04 18:47:38 +0300

    Update favicon

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,22 @@
 import { Inter } from "next/font/google";
+import Head from "next/head"; // Make sure to import Head
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Nuclearn",
-  description: "All the resouce you need about Nuclear Energy",
+  description: "All the resources you need about Nuclear Energy",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
